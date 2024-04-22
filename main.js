5abe0800e68bb887bd60e8fa7dbfdcecfa4e6163
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-04-22 10:46:12 -0300

    Iniciando com o JavaScript

---
+++ main.js
@@ -0,0 +1,53 @@
+//Exercício 1 - Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador
+// alert("Hello World!")
+
+//Exercício 2
+// let a = 10
+// let b = 20
+// let result = a + b
+// alert(result)
+
+//Exercício 3
+// let a = '10'
+// let result = typeof a == 'number' ? 'É um número' : 'Não é um número'
+// alert(result)
+
+//Exercício 4
+// let a = 10
+// let result = typeof a != 'number' ? 'É uma string' : 'Não é uma string'
+// alert(result)
+
+//Exercício 5
+// let a = true
+// let result = typeof a == 'boolean' ? 'É um booleano' : 'Não é um booleano'
+// alert(result)
+
+//Exercício 6
+// let a = 10
+// let b = 20
+// let result = a - b
+// alert(result)
+
+//Exercício 7
+// let a = 10
+// let b = 20
+// let result = a * b
+// alert(result)
+
+//Exercício 8
+// let a = 10
+// let b = 20
+// let result = a / b
+// alert(result)
+
+//Exercício 9
+// let a = 15
+// let result = a%2 == 0 ? 'É número é par' : 'Não é um número par'
+// alert(result)
+
+//Exercício 10
+let a = 11
+let result = a%2 != 0 ? 'É número é ímpar' : 'Não é um número ímpar'
+alert(result)
+
+
